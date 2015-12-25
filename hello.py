f338b762750f9185fd77cd04f1539be3d2a6e5f9
Assaf <assafdk@gmail.com>
2015-12-25 13:13:25 +0200

    catch Google's code and error

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,4 +10,9 @@ def hello():
 
 @app.route('/google-adwords/oauth2callback')
 def hello_ggl():
-    return 'Hello Google!'
+    error = request.args.get('error','') 
+    if error != '':
+    	return "Google returned the following error: " + error
+    code = request.args.get('code')
+    #ret_str = 'Hello Google! Your code is'
+    return error#ret_str
