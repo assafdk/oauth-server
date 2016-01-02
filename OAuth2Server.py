93368e66fcc95352885da7e79ef9dacda151c246
Yanir Calisar <yanir-pro@Yanirs-MacBook-Pro.local>
2016-01-02 12:40:14 +0200

    test Parse

--- OAuth2Server.py
+++ OAuth2Server.py
@@ -66,10 +66,10 @@ def oauth2callback():
   Parse.register(APPLICATION_ID, REST_API_KEY)
   Parse.pushTokens(userId = account_id, gglAccessToken = access_token, gglRefreshToken = refresh_token)
   #return "Parse OK"
-  return "Account: " + account_id + "\n Access token: " + access_token + "\nRefresh token: " + refresh_token + "\n\n were pushed to Parse"
+  #return "Account: " + account_id + "\n Access token: " + access_token + "\nRefresh token: " + refresh_token + "\n\n were pushed to Parse"
   return_url = HOME_PAGE_3TARGETING + "?status=ok"
-  #return redirect(return_url)
-  return ("Access token for user " + account_id + " is " + access_token)
+  return redirect(return_url)
+  #return ("Access token for user " + account_id + " is " + access_token)
 
   #"Access token = " + access_token + "Refresh token = " + refresh_token
   #return flask.redirect(flask.url_for('index'))
