09f6172be4cb8fbfd91e29d304b016044c757f0c
Yanir Calisar <yanir-pro@Yanirs-MacBook-Pro.local>
2016-03-28 16:35:18 +0300

    copied mySQLreplica.py

---
+++ mySQLreplica.py
@@ -0,0 +1,167 @@
+__author__ = 'assafdekel'
+import json
+import mysql.connector
+
+class Database:
+
+    cnx = mysql.connector
+
+    def __init__(self):
+        self.cnx = mysql.connector.connect(user='locatit6_3yc', password='ShedShvil11',
+                                           host='50.87.248.155',
+                                           database='locatit6_3targeting')
+    def __del__(self):
+        self.cnx.close()
+
+    def closeConnection(self):
+        self.cnx.close()
+
+    def getSalesforceCredentials(self, accountId):
+        cursor = self.cnx.cursor()
+        query = ("SELECT consumer_key, consumer_secret, access_token, instance_url, refresh_token, sf_id, issued_at, scope, signature, token_type, enabled FROM sf_credentials WHERE account_id = {}").format(accountId)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        colNames = cursor.column_names
+
+        for row in result:
+            sfCred = {} # credentials dictionary
+            sfCred['accountId'] = accountId
+            sfCred['consumer_key'] = row[0]
+            sfCred['consumer_secret'] = row[1]
+            sfCred['access_token'] = row[2]
+            sfCred['instance_url'] = row[3]
+            sfCred['refresh_token'] = row[4]
+            sfCred['sf_id'] = row[5]
+            sfCred['issued_at'] = row[6]
+            sfCred['scope'] = row[7]
+            sfCred['signature'] = row[8]
+            sfCred['token_type'] = row[9]
+            sfCred['enabled'] = row[10]
+
+        # for account in cursor:
+        #     sf_access_token = account.sfdc_access_token
+        #     sf_refresh_token = sfdc_refresh_token
+        #     sf_id = sfdc_id
+        #     sf_instance_url = sfdc_instance_url
+
+
+        cursor.close()
+        return sfCred
+
+
+    def getAdwordsCredentials(self, accountId):
+        cursor = self.cnx.cursor()
+        query = ("SELECT enabled, access_token, refresh_token FROM adwords_credentials WHERE account_id = {}").format(accountId)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        colNames = cursor.column_names
+
+        for row in result:
+            adwordsCred = {} # credentials dictionary
+            adwordsCred['enabled'] = row[0]
+            adwordsCred['access_token'] = row[1]
+            adwordsCred['refresh_token'] = row[2]
+
+        cursor.close()
+        return adwordsCred
+
+    def getFacebookCredentials(self, accountId):
+        cursor = self.cnx.cursor()
+        query = ("SELECT enabled, fb_user_id, fb_account_id, user_token, expiresIn, spent FROM fb_credentials WHERE account_id = {}").format(accountId)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        colNames = cursor.column_names
+
+        for row in result:
+            fbCred = {} # credentials dictionary
+            fbCred['enabled'] = row[0]
+            fbCred['fb_user_id'] = row[1]
+            fbCred['fb_account_id'] = row[2]
+            fbCred['user_token'] = row[3]
+            fbCred['expiresIn'] = row[4]
+            fbCred['spent'] = row[5]
+
+        cursor.close()
+        return fbCred
+
+    def pushSalseforceCredentials(self, sfCred):
+        cursor = self.cnx.cursor()
+        query = ("UPDATE sf_credentials SET access_token = '" + sfCred['access_token']
+                 + "', refresh_token = '" + sfCred['refresh_token']
+                 + "', instance_url = '" + sfCred['instance_url']
+                 + "', sf_id = '" + sfCred['sf_id']
+                 + "', issued_at = '" + sfCred['issued_at']
+                 + "', scope = '" + sfCred['scope']
+                 + "', signature = '" + sfCred['signature']
+                 + "', token_type = '" + sfCred['token_type']
+                 + "' WHERE account_id = {}").format(sfCred['accountId'])
+        cursor.execute(query)
+        self.cnx.commit()
+        cursor.close()
+        return
+
+    def pushFacebookCredentials(self, fbCred):
+        cursor = self.cnx.cursor()
+        query = ("UPDATE fb_credentials SET user_token = '" + fbCred['user_token']
+                 + "', expiresIn = '" + fbCred['expiresIn']
+                 + "', spent = '" + fbCred['spent']
+                 + "' WHERE account_id = {}").format(fbCred['accountId'])
+
+        cursor.execute(query)
+        self.cnx.commit()
+        cursor.close()
+        return
+
+    def pushAdwordsCredentials(self, awCred):
+        cursor = self.cnx.cursor()
+        query = "UPDATE adwords_credentials SET access_token = '" + awCred['access_token'] \
+                + "', refresh_token = '" + awCred['refresh_token'] \
+                + "' WHERE account_id = {}".format(awCred['accountId'])
+        cursor.execute(query)
+        self.cnx.commit()
+        cursor.close()
+        return
+
+    # def pushTwitterCredentials(self, accountId, twitterCred):
+    #     cursor = self.cnx.cursor()
+    #     query = ("UPDATE accounts SET fb_user_id=twitterCred['fb_user_id'], fb_account_id=twitterCred['fb_account_id'], fb_user_token=twitterCred['fb_user_token'], fb_expiresIn=twitterCred['fb_expiresIn'], fb_spent=twitterCred['fb_spent'] WHERE id = {}").format(accountId)
+    #     cursor.execute(query)
+    #     cursor.close()
+    #     return
+
+
+    def query(self, queryStr):
+        cursor = self.cnx.cursor()
+        cursor.execute(queryStr)
+
+        cursor.close()
+        self.cnx.close()
+
+# debug
+# a = Database()
+# accountId = 1
+#
+# sfCred = {}
+# sfCred['accountId'] = '1'
+# sfCred['access_token'] = '1'
+# sfCred['refresh_token'] = '1'
+# sfCred['instance_url'] = '1'
+# sfCred['sf_id'] = '1'
+# sfCred['issued_at'] = '1'
+# sfCred['scope'] = '1'
+# sfCred['signature'] = '1'
+# sfCred['token_type'] = '1'
+#
+# fbCred = {}
+# fbCred['accountId'] = '1'
+# fbCred['user_token'] = '1'
+# fbCred['expiresIn'] = '1'
+# fbCred['spent'] = '1'
+#
+# awCred = {}
+# awCred['accountId'] = '1'
+# awCred['access_token'] = '1'
+# awCred['refresh_token'] = '1'
+#
+# print a
+#
